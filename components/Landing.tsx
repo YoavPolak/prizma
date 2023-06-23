fd43ba2af1848a48af6191d0394ba83fc788b9fb
YoavPol <bluekudeq100@gmail.com>
2023-06-23 18:58:48 +0300

    fixed

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -71,11 +71,11 @@ export default function Landing () {
           YOAV POLAK
         </h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
-          I'm a Full Stack Developer.
+          I&apos;m a Full Stack Developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m a full-stack developer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
+          I&apos;m a full-stack developer specializing in building (and occasionally
+          designing) exceptional digital experiences. Currently, I&apos;m focused on
           building responsive full-stack web applications.
         </p>
         </div>
